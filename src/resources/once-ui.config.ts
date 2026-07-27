f40c85f6c0ea88615d70c46a3264139605b49c5b
Domenico Futia <domenicofutia00@gmail.com>
2026-07-27 14:38:29 +0200

    disable /work route and remove demo protected route

--- src/resources/once-ui.config.ts
+++ src/resources/once-ui.config.ts
@@ -20,7 +20,7 @@ const routes: RoutesConfig = {
   "/": true,
   "/about": true,
   "/esn": true,
-  "/work": true,
+  "/work": false,
   "/blog": true,
   "/gallery": false,
 };
@@ -33,9 +33,7 @@ const display: DisplayConfig = {
 
 // Enable password protection on selected routes
 // Set password in the .env file, refer to .env.example
-const protectedRoutes: ProtectedRoutesConfig = {
-  "/work/automate-design-handovers-with-a-figma-to-code-pipeline": true,
-};
+const protectedRoutes: ProtectedRoutesConfig = {};
 
 // Import and set font for each variant
 import { Geist } from "next/font/google";
