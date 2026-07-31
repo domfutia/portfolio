43a033fbd420ec4dfd7220b28f89833520f6379a
Domenico Futia <domenicofutia00@gmail.com>
2026-07-31 18:24:12 +0200

    fix: update brand and accent colors in style configuration

--- src/resources/once-ui.config.ts
+++ src/resources/once-ui.config.ts
@@ -74,8 +74,8 @@ const fonts: FontsConfig = {
 const style: StyleConfig = {
   theme: "light", // dark | light | system
   neutral: "gray", // sand | gray | slate | mint | rose | dusk | custom
-  brand: "violet", // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan | custom
-  accent: "pink", // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan | custom
+  brand: "cyan", // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan | custom
+  accent: "aqua", // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan | custom
   solid: "contrast", // color | contrast
   solidStyle: "flat", // flat | plastic
   border: "playful", // rounded | playful | conservative | sharp
